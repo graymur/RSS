60b7a2c51bbb3ab07f460b1e50cae4fd463ea581
graymur <graymur@mail.ru>
2016-12-30 08:27:38 +0100

    cleaned up server code

--- server/api/1/index.js
+++ server/api/1/index.js
@@ -3,17 +3,6 @@ const runGenerator = require('./util/runGenerator.js');
 const express = require('express');
 const router = express.Router();
 
-const dv = function (v) { console.log(JSON.stringify(v, null, 4)); };
-
-const url = 'http://feeds.feedburner.com/bigpictures';
-
-const parser = require('rss-parser');
-
-const FeedModel = require('./models/feed.js').model;
-const PostModel = require('./models/post.js').model;
-const UserModel = require('./models/user.js').model;
-const GroupModel = require('./models/group.js').model;
-
 router.use(require('./middleware/user.js'));
 
 router.get('/feeds', require('./endpoints/feeds.js'));
@@ -23,9 +12,5 @@ router.get('/fetch', require('./endpoints/fetch.js'));
 router.get('/update', require('./endpoints/update.js'));
 router.put('/save', require('./endpoints/save.js'));
 router.post('/mark-read', require('./endpoints/mark-read.js'));
-
-router.get('/about', function(req, res) {
-    res.send('About birds');
-});
 
 module.exports = router;
