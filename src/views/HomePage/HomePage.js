b5020a9c5bdd91db39d32ed35261f4e127396075
graymur <graymur@mail.ru>
2017-06-19 19:57:52 +0200

    Dependencies versions updates and removed redundant action

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import Resizeable from 'components/Resizeable/Resizeable';
 import Feeds from 'components/Feeds/Feeds';
@@ -10,74 +10,71 @@ import Post from 'components/Post/Post';
 
 import * as selectors from './selectors.js';
 
-import { fetchFeeds, updateFeed } from './actions.js';
+import {fetchFeeds} from './actions.js';
 
 import './home.scss';
 
 export class HomePage extends React.Component {
-    static propTypes = {
-        feedsByGroups: PropTypes.array.isRequired,
-        fetchFeeds: PropTypes.func.isRequired
-    };
+	static propTypes = {
+		feedsByGroups: PropTypes.array.isRequired,
+		fetchFeeds: PropTypes.func.isRequired
+	};
 
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            widths: [25, 25]
-        };
+		this.state = {
+			widths: [25, 25]
+		};
 
-        this.dragHandlers = this.state.widths.map((width, index) => {
-            return (position, element) => this.onDrag(index, position, element);
-        });
-    }
+		this.dragHandlers = this.state.widths.map((width, index) => {
+			return (position, element) => this.onDrag(index, position, element);
+		});
+	}
 
-    componentWillMount() {
-        this.fetchFeeds();
-    }
+	componentWillMount() {
+		this.fetchFeeds();
+	}
 
-    fetchFeeds() {
-        if (!this.props.feedsByGroups.length) {
-            this.props.fetchFeeds();
-        }
-    }
+	fetchFeeds() {
+		if (!this.props.feedsByGroups.length) {
+			this.props.fetchFeeds();
+		}
+	}
 
-    onDrag(index, position, element) {
-        const viewportOffset = element.getBoundingClientRect();
-        const newWidth = Math.round((position - viewportOffset.left) / window.innerWidth * 100);
+	onDrag(index, position, element) {
+		const viewportOffset = element.getBoundingClientRect();
+		const newWidth = Math.round((position - viewportOffset.left) / window.innerWidth * 100);
 
-        if (newWidth < 5) return;
+		if (newWidth < 5) return;
 
-        const newWidths = this.state.widths.map(x => x);
-        newWidths[index] = newWidth;
+		const newWidths = this.state.widths.map(x => x);
+		newWidths[index] = newWidth;
 
-        this.setState({ widths: newWidths });
-    }
+		this.setState({widths: newWidths});
+	}
 
-    render() {
-        const { feedsByGroups } = this.props;
+	render() {
+		const {feedsByGroups} = this.props;
 
-        return (
-            <div className='content'>
-                <Resizeable className='block' width={this.state.widths[0]} onDrag={this.dragHandlers[0]}>
-                    <Feeds feeds={feedsByGroups} width={this.state.widths[0]} />
-                </Resizeable>
-                <Resizeable className='block' width={this.state.widths[1]} onDrag={this.dragHandlers[1]}>
-                    <Feed width={this.state.widths[1]} />
-                </Resizeable>
-                <div className='block'>
-                    <Post width={this.state.widths[0] + this.state.widths[1]} />
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className='content'>
+				<Resizeable className='block' width={this.state.widths[0]} onDrag={this.dragHandlers[0]}>
+					<Feeds feeds={feedsByGroups} width={this.state.widths[0]}/>
+				</Resizeable>
+				<Resizeable className='block' width={this.state.widths[1]} onDrag={this.dragHandlers[1]}>
+					<Feed width={this.state.widths[1]}/>
+				</Resizeable>
+				<div className='block'>
+					<Post width={this.state.widths[0] + this.state.widths[1]}/>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = createStructuredSelector({
-    feedsByGroups: selectors.selectFeedsByGroups()
+	feedsByGroups: selectors.selectFeedsByGroups()
 });
 
-export default connect(mapStateToProps, {
-    fetchFeeds,
-    updateFeed
-})(HomePage);
+export default connect(mapStateToProps, {fetchFeeds})(HomePage);
